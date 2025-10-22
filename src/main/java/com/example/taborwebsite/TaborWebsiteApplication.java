082e0ce894473331647be25a87eac1c6a89ec9a6
Connor Evans <connor.l.evans.mil@army.mil>
2025-10-21 21:04:32 -0500

    [1] adds initial frontend and backend scaffolding for tabors website

---
+++ src/main/java/com/example/taborwebsite/TaborWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.taborwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaborWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaborWebsiteApplication.class, args);
+    }
+
+}
